df7a14b79f38de2eb1113378a0601e5a63bc05f7
Harry-Ramsey <108581419+Harry-Ramsey@users.noreply.github.com>
2022-07-23 10:35:28 +0100

    Upload initial files
    Upload initial files containing source code to generate a one-time-password.

---
+++ Autenticators/HOTP.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Autenticators
+{
+    class HOTP : OTP
+    {
+
+        public static long Counter { private get; set; } = 0;
+
+        public HOTP(byte[] Key, int CodeLength = 6) : base(Key, CodeLength) { }
+
+        public override string GenerateOTP()
+        {
+            // We need to convert the counter into a BigEndian byte array..
+            byte[] buffer = ConvertLongToBigEndianBytes(Counter);
+            // We can then HMAC the counter using their key.
+            byte[] hash = GenerateHMACHash(buffer);
+            // We trunacate the hash into a string code.
+            string otp = TrunacateOTPToString(hash);
+            // We manually increment the counter.
+            Counter++;
+            return otp;
+        }
+    }
+}
---
+++ Autenticators/TOTP.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Numerics;
+using System.Security.Cryptography;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Autenticators
+{
+    class TOTP : OTP
+    {
+        public static int TimeFrame { get; set; } = 30;
+
+        public TOTP(byte[] Key, int OTPLength = 8) : base(Key, OTPLength) {   }
+
+        public override string GenerateOTP()
+        {
+            // We need to convert the time divided by the timeframe into a BigEndian byte array.
+            byte[] buffer = ConvertLongToBigEndianBytes(GetCurrentTimeInSeconds() / TimeFrame);
+            // We can then HMAC the counter using their key.
+            byte[] hash = GenerateHMACHash(buffer);
+            // We trunacate the hash into an integer and pad the front with zeros.
+            string otp = TrunacateOTPToString(hash);
+            return otp;
+        }
+
+        private long GetCurrentTimeInSeconds()
+        {
+            long seconds = (long)(DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalSeconds;
+            return seconds;
+        }
+
+        public long GetTimeTillChangeInSeconds()
+        {
+            long seconds = (long)(TimeFrame - (GetCurrentTimeInSeconds() % TimeFrame));
+            return seconds;
+        }
+    }
+}
